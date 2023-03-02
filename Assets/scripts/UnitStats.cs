a982f0bac4e0a9fcbb98c456cfba5d882a96458d
omar <omarvalle2000@gmail.com>
2023-03-02 14:53:51 -0800

    delete scripts

--- Assets/scripts/UnitStats.cs
+++
@@ -1,9 +0,0 @@
-public class UnitStats : MonoBehaviour 
-{
-    public float health;
-    public float mana;
-    public float attack;
-    public float magic;
-    public float defense;
-    public float speed;
-}
