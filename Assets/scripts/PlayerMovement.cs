cf5254d33bf480f2ad7bff3cc3bf2ca35571546b
Tim Chester <tjchester7@gmail.com>
2023-03-04 01:05:16 -0800

    Character prefab and movement
    movement with animations. Added code to player movement script

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,41 +8,94 @@ public class PlayerMovement : MonoBehaviour
     private float speed;
     [SerializeField]
     private Animator animator;
-    void FixedUpdate()
+    [SerializeField]
+    Rigidbody2D rb;
+
+    public int currentHealth;
+    public int maxHealth = 20;
+    public int str = 1;
+
+    public int exp;
+    public int expThreshold = 100;
+    public int healthIncrease = 5;
+    public int strIncrease = 1;
+
+    private float moveHorizontal, moveVertical;
+
+    private void Start()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody2D>();
+
+        // Probably need to change these for scene transitions
+        currentHealth = maxHealth; 
+        exp = 0;
+    }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        // Animation variable code
         Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-        float newVelocityX = 0f;
+
         if (moveHorizontal < 0 && currentVelocity.x <= 0)
         {
-            newVelocityX = -speed;
             animator.SetInteger("DirectionX", -1);
         }
         else if (moveHorizontal > 0 && currentVelocity.x >= 0)
         {
-            newVelocityX = speed;
             animator.SetInteger("DirectionX", 1);
         }
         else
         {
             animator.SetInteger("DirectionX", 0);
         }
-        float newVelocityY = 0f;
         if (moveVertical < 0 && currentVelocity.y <= 0)
         {
-            newVelocityY = -speed;
             animator.SetInteger("DirectionY", -1);
         }
         else if (moveVertical > 0 && currentVelocity.y >= 0)
         {
-            newVelocityY = speed;
             animator.SetInteger("DirectionY", 1);
         }
         else
         {
             animator.SetInteger("DirectionY", 0);
         }
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, newVelocityY);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void addExp(int expGains)
+    {
+        exp += expGains;
+
+        if (exp >= expThreshold)
+        {
+            levelUp();
+        }
+    }
+
+    public void levelUp()
+    {
+        maxHealth += healthIncrease;
+        str += strIncrease;
+
+        // Get leftover exp to keep towards next level up
+        exp -= expThreshold;
     }
 }
