ae4b7f8b023b058367c5166b0f53c18679f07570
Tim Chester <tjchester7@gmail.com>
2023-03-10 11:29:14 -0800

    Attack and Enemy
    added attack animation for down, code for attacking, and enemy script for movement

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Rigidbody2D rb;
 
+    public bool isAttacking = false;
+    private Collider2D hitBox;
+    public Vector2 hitBoxSize = new Vector2(.5f, .5f), hitBoxLocation;
+
     public int currentHealth;
     public int maxHealth = 20;
     public int str = 1;
@@ -21,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     public int strIncrease = 1;
 
     private float moveHorizontal, moveVertical;
+    Vector2 currentVelocity;
 
     private void Start()
     {
@@ -33,41 +38,42 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
-        moveVertical = Input.GetAxisRaw("Vertical");
-
-        // Animation variable code
-        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-
-        if (moveHorizontal < 0 && currentVelocity.x <= 0)
+        if (!isAttacking)
         {
-            animator.SetInteger("DirectionX", -1);
-        }
-        else if (moveHorizontal > 0 && currentVelocity.x >= 0)
-        {
-            animator.SetInteger("DirectionX", 1);
+            moveHorizontal = Input.GetAxisRaw("Horizontal");
+            moveVertical = Input.GetAxisRaw("Vertical");
         }
         else
         {
-            animator.SetInteger("DirectionX", 0);
+            moveHorizontal = 0f;
+            moveVertical = 0f;
         }
-        if (moveVertical < 0 && currentVelocity.y <= 0)
-        {
-            animator.SetInteger("DirectionY", -1);
-        }
-        else if (moveVertical > 0 && currentVelocity.y >= 0)
-        {
-            animator.SetInteger("DirectionY", 1);
-        }
-        else
-        {
-            animator.SetInteger("DirectionY", 0);
-        }
+
+
+        attack();
+        animate();
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        if (!isAttacking)
+        {
+            rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    
+
+    public void attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 
     public void takeDamage(int damage)
@@ -97,5 +103,49 @@ public class PlayerMovement : MonoBehaviour
 
         // Get leftover exp to keep towards next level up
         exp -= expThreshold;
+    }
+
+    public void animate()
+    {
+        // Animation variable code
+        currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+        if (moveHorizontal < 0 && currentVelocity.x <= 0)
+        {
+            animator.SetInteger("DirectionX", -1);
+        }
+        else if (moveHorizontal > 0 && currentVelocity.x >= 0)
+        {
+            animator.SetInteger("DirectionX", 1);
+        }
+        else
+        {
+            animator.SetInteger("DirectionX", 0);
+        }
+        if (moveVertical < 0 && currentVelocity.y <= 0)
+        {
+            animator.SetInteger("DirectionY", -1);
+        }
+        else if (moveVertical > 0 && currentVelocity.y >= 0)
+        {
+            animator.SetInteger("DirectionY", 1);
+        }
+        else
+        {
+            animator.SetInteger("DirectionY", 0);
+        }
+    }
+
+    public void isAttackingOn()
+    {
+        //Debug.Log("isAttackingOn()");
+        isAttacking = true;
+        Invoke("isAttackingOff", 1f); // Failsafe
+    }
+
+    public void isAttackingOff()
+    {
+        //Debug.Log("isAttackingOff()");
+        isAttacking = false;
     }
 }
