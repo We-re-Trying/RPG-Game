ad78f94e3e2bc5cde6d0229b3d542eb29e116f2c
omarv10 <omarvalle2000@gmail.com>
2023-05-04 13:10:29 -0700

    cleaned up yeaston map

--- Assets/scripts/EntranceScript.cs
+++ Assets/scripts/EntranceScript.cs
@@ -7,6 +7,7 @@ public class EntranceScript : MonoBehaviour
 {
     public GameManager gameManager;
     public SaveLoadManager sl;
+    public int floor = 2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -14,7 +15,7 @@ public class EntranceScript : MonoBehaviour
             if (sl && gameManager)
             {
                 sl.saveGame();
-                SceneManager.LoadScene(gameManager.currentFloor);
+                SceneManager.LoadScene(floor);
             }
         }
     }
