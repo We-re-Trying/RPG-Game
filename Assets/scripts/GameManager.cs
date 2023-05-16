ff739f0f4800e392506a4997be86c814ccea2ea3
Aldorithms <99375833+aldorithms@users.noreply.github.com>
2023-05-16 11:05:21 -0700

    catch
    detered

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isBattle = false;
+    public int currentFloor = 2;
+    public SaveLoadManager sl;
+    public PlayerMovement max, lucia;
+    public void Awake()
+    {
+        sl.loadGame();
+        max.refillHealth();
+        lucia.refillHealth();
+    }
+
+    public bool getIsBattle()
+    {
+        return isBattle;
+    }
+}
+
